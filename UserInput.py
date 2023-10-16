b5cca087d72be59a6f3e2debc963e6ed2b12802c
Ashar Kamal <ashar.kamal98@gmail.com>
2023-10-16 14:38:39 +0200

    Added real singlet extended SM as example

--- UserInput.py
+++ UserInput.py
@@ -1,9 +1,9 @@
 
 ############################################################### BSM DETAILS ########################################################
 #===================================================================================================================================
-num_h = "3"     # Use citation marks 
-num_hp = "1"    # Use citation marks
-exp_num_training_points = 19 #15 #20=19h #16=2h
+num_h = "2"     # Use citation marks 
+num_hp = "0"    # Use citation marks
+exp_num_training_points = 14 #15 #20=19h #16=2h
 num_training_points = 2**(exp_num_training_points)
 exp_num_pred_points = 10
 num_pred_points = 2**(exp_num_pred_points)
@@ -22,8 +22,8 @@ d_TSM = { \
 
 
 
-BSM_model = "THDM"
-d = { \
+#BSM_model = "THDM"
+d_THDM = { \
 
     "Parameter name": ['M11','M22','M12','lam1','lam2','lam3','lam4','lam5','TanBeta','mC','mA','mh','mH', 'v1', 'v2'], \
 
@@ -43,6 +43,28 @@ d = { \
                     "(-mA**2+mh**2+mH**2-2*lam1*v1**2-2*lam2*v2**2)/(2*(v1**2+v2**2))",
                     None, None, None, None, None, "v*np.cos(np.arctan(TanBeta))", "v*np.sin(np.arctan(TanBeta))"], \
     "Solve order" : [3, 3, 2, None, None, 2, 2, 2, None, None, None, None, None, 1, 1]
+
+    }
+
+
+BSM_model = "SSM"
+d = { \
+
+    "Parameter name": ['mu2','MS','K1','kappa','lam','K2','lamS','vS','mh','mH'], \
+
+    "LesHouches number": [1, 2, 3, 4, 5, 6, 7, 8, None, None], \
+
+    "Range start" : [None, None, None, None, -12, -12, -12, 50, 125.25, 125.25], \
+
+    "Range end" :   [None, None, None,  None,  12, 12, 12, 1000, 125.25, 2000], \
+
+    "Dependence" : ["-(1/2) * (2*K1*vS + K2*vS**2 + lam*v**2)",
+                    "-(1/(2*vS)) * (2*kappa*vS**2 + 4*lamS*vS**3 + K1*v**2 + K2*vS*v**2)",
+                    #"-(1/(2*vS)) * (2*kappa*vS**2)",
+                    "-K2*vS - (1/(v**2)) * cmath.sqrt(-v**2 * (mh**2-lam*v**2) * (mH**2-lam*v**2))",
+                    "-(1/(2*vS**2)) * (-2*vS*(mh**2 + mH**2 - 4*lamS*vS**2) + (K2 + 2*lam)*vS*v**2 + cmath.sqrt(-v**2 * (mh**2-lam*v**2) * (mH**2-lam*v**2)))",
+                    None, None, None, None, None, None], \
+    "Solve order" : [2, 2, 1, 1, None, None, None, None, None, None]
 
     }
 
